ec117d4113385a76fb25da2e4ebe43c051c14b43
Pedro Lima <psantoslima86@gmail.com>
2022-03-31 17:45:10 -0300

    pedro
    javascript está recebendo o valor dos inputs

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-function calcularJantar(){
-    var totalConta = document.getElementById("valorPedido").value;
-    var taxaServico = document.getElementsByClassName("bt-gorjeta").value;
-    var numeroPessoas = document.getElementById("qtdPessoas").value;
+// PEGAR VALORES
+  const totalPedido = document.getElementById("valorPedido"); //Valor total do pedido
+ const gorjetaPorcentagemBotao = document.querySelector('input[name="valorGorjeta"]:checked')
+  const gorjetaPorcentagemPersonal = document.getElementById("gorjetaPersonalizada") //Valor da porcentagem personalizada do pedido
+  const numeroPessoas = document.getElementById("qtdPessoas"); //Quantidade de pessoas que irão pagar
+  const valorGorjetaTotal = document.getElementById("totalGorjeta"); //Valor a ser exibido da gorjeta total
+  const valorGorjetaPessoa = document.getElementById("gorjetaPessoa"); //Valor a ser exibido da gorjeta por pessoa
+    var form = document.querySelector('#enviarForm');
+    var botao = document.querySelector('#calcular');
 
-    if(!totalConta|| !taxaServico|| !numeroPessoas){
-        alert("Por favor, coloque um valor válido!")
-        return;
-    }
+        botao.addEventListener('click' , function(event) {
+            // event.preventDefault();
+             console.log("A porcentagem escolhida foi de: " , form.valorGorjeta.value , "%");
+            console.log("O valor total do pedido em R$ foi de: " , totalPedido.value);
+            console.log("O total de pessoas que irão pagar é de: " , numeroPessoas.value);
 
-}
-document.getElementById("calcular").onclick = function(){
-    e.preventDefault();
-    calcularJantar();
-}
+        });
+
+  
+
+
+
+//   var textoGorjetaPessoa = document.getElementById("gorjetaPessoa").value;
+
+//   var valorPessoa = totalConta / numeroPessoas;
+
+//   textoGorjetaPessoa.innerText = textoGorjetaPessoa;
+
+//   console.log(textoGorjetaPessoa);
+//   console.log("Valor da conta por pessoa é:", valorPessoa);
+// }
+
+// document.getElementById("enviarForm").onsubmit = function (e) {
+//   e.preventDefault();
+//   calcularJantar();
+// };
