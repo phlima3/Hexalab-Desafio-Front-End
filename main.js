c6530f0ef2719092fdefac15e49ed5414e1978a8
Pedro Lima <psantoslima86@gmail.com>
2022-04-05 15:31:47 -0300

    pedro
    Verificação dos inputs e exibição dos erros para verificar se estão selecionados ou não.

--- main.js
+++ main.js
@@ -11,16 +11,51 @@ const numeroPessoas = document.querySelector("#qtdPessoas");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const radiosValue = [];
+
 
   inputs.forEach((input) => {
-    if (!input.value) {
+    
+    if(input.getAttribute("class") === "bt-gorjeta"){
+      radiosValue.push(input.checked);       
+    }
+  
+    else if (!input.value){
       input.className = "erro";
+      
       return;
     }
 
     if (input.classList.contains("erro")) {
       input.classList.remove("erro");
     }
-  });
+  })
+  if(radiosValue.indexOf(true) >= 0 ){
+    console.log("Entrei aqui")
+    if(porcentagemCustom.value != '') {
+    porcentagemCustom.className = "erro";
+      console.log("Com valor no personalizado")
+    }
+    else {
+      porcentagemCustom.classList.remove("erro");
+    }
+
+
+  }
+
+
+  console.log(radiosValue);
 });
+
+// setInterval(()=> {
+  
+  
+//   console.log(porcentagemBotao);
+
+// },1000);
+ 
+
+
+
+
  
