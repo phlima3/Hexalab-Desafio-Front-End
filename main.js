43500fead6c4b78991000f701cb4eccf2a3758be
Pedro Lima <psantoslima86@gmail.com>
2022-04-07 15:37:24 -0300

    pedro
    JS calcula todos os valores, ainda com auxilio do botao

--- main.js
+++ main.js
@@ -8,40 +8,75 @@ const porcentagemBotao = document.querySelector(
   'input[name="valorGorjeta"]:checked'
 );
 const porcentagemCustom = document.querySelector("#gorjetaPersonalizada");
+
 const pessoas = document.querySelector("#qtdPessoas");
 
 //RESULTADO
 const resultadoConta = document.querySelector("#totalGorjeta");
 const resultadoPessoa = document.querySelector("#gorjetaPessoa");
 
-//VERIFICAR OS INPUTSå
+let erro = false;
+
+let porcentagemSelected = 1;
+
+//VERIFICAR OS INPUTS
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  resultadoConta.innerHTML = (total.value * porcentagemCustom.value) / 100;
-  resultadoPessoa.innerHTML =
-    (total.value * porcentagemCustom.value) / 100 / pessoas.value;
 
   const radiosValue = [];
 
   inputs.forEach((input) => {
     if (input.getAttribute("class") === "bt-gorjeta") {
       radiosValue.push(input.checked);
-    } else if (!input.value) {
+
+      if (input.checked) {
+        porcentagemSelected = input.value;
+      }
+    } else if (!input.value || input.value <= 0) {
       input.className = "erro";
+      erro = true;
 
       return;
     }
 
     if (input.classList.contains("erro")) {
       input.classList.remove("erro");
+      erro = false;
     }
   });
   if (radiosValue.indexOf(true) >= 0) {
     if (porcentagemCustom.value != "") {
       porcentagemCustom.className = "erro";
       console.log("Com valor no personalizado");
+      erro = true;
     } else {
       porcentagemCustom.classList.remove("erro");
+      erro = false;
     }
   }
+  if (!erro) {
+    calcularTotal();
+  }
+
+  console.log(erro);
 });
+
+function calcularTotal() {
+  let valorRadioSelected = !porcentagemSelected ? 1 : porcentagemSelected;
+  let valorPersonalizado = !porcentagemCustom.value
+    ? 1
+    : porcentagemCustom.value;
+
+  console.log(valorRadioSelected);
+  console.log(valorPersonalizado);
+
+  resultadoConta.innerHTML = (
+    (total.value * valorRadioSelected * valorPersonalizado) /
+    100
+  ).toLocaleString("pt-BR", { minimumFractionDigits: 2 });
+  resultadoPessoa.innerHTML = (
+    (total.value * valorRadioSelected * valorPersonalizado) /
+    100 /
+    pessoas.value
+  ).toLocaleString("pt-BR", { minimumFractionDigits: 2 });
+}
